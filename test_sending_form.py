a9d3475467792c4a9e637cb6281e0eb21a2df691
Alexey <ronski941@gmail.com>
2020-01-31 01:45:05 +0300

    added 2 tests to script

--- test_sending_form.py
+++ test_sending_form.py
@@ -19,6 +19,7 @@ appartment = '5'
 
 @pytest.mark.positive_required_fields
 def test_positive_required_fields_filled(browser):
+
     browser.get(link)
     browser.implicitly_wait(5)
 
@@ -46,6 +47,7 @@ def test_positive_required_fields_filled(browser):
 
 @pytest.mark.positive_all_fields
 def test_positive_all_fields_filled(browser):
+
     browser.get(link)
     browser.implicitly_wait(5)
 
@@ -91,3 +93,50 @@ def test_positive_all_fields_filled(browser):
 
     assert 'правительство' in head_feedback, 'У правительства обед, обращение не зарегистрировано'
 
+
+@pytest.mark.positive_transfer_lines_fio
+def test_positive_transfer_lines_fio(browser):
+
+    browser.get(link)
+    browser.implicitly_wait(5)
+
+    lastname_field = browser.find_element_by_id("fullname-surname")
+    lastname_field.send_keys(lastname)
+
+    name_field = browser.find_element_by_id("fullname-name")
+    name_field.send_keys(name)
+
+    patronymic_field = browser.find_element_by_id("fullname-patronymic")
+    patronymic_field.send_keys(patronymic)
+
+    fullname_field = browser.find_element_by_id('fullname')
+    fullname_text = fullname_field.get_attribute('value')
+
+    assert fullname_text == f'{lastname} {name}', 'Поле полного ФИО не совпадает со строчными полями'
+
+
+@pytest.mark.positive_transfer_lines_address
+def test_positive_transfer_lines_address(browser):
+
+    browser.get(link)
+    browser.implicitly_wait(5)
+    city_field = browser.find_element_by_id('address-city')
+    city_field.send_keys(city)
+
+    street_field = browser.find_element_by_id('address-street')
+    street_field.send_keys(street)
+
+    building_field = browser.find_element_by_id('address-house')
+    building_field.send_keys(building)
+
+    appartment_field = browser.find_element_by_id('address-flat')
+    appartment_field.send_keys(appartment)
+
+    next_field = browser.find_element_by_id("message")
+    next_field.send_keys('')
+
+    fulladdress_field = browser.find_element_by_id('address')
+    fulladdress_text = fulladdress_field.get_attribute('value')
+
+    assert fulladdress_text == f'{city}, {street}, {building}, {appartment}', 'Поле полного адреса не совпадает со ' \
+                                                                              'строчными полями'
